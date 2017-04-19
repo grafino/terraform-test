ebf81bcf04a47d24f9fd339e75859767a9c634ae
Rui Grafino <rui.grafino@marionete.co.uk>
2017-04-19 17:26:15 +0100

    Improve main.tf for several servers

--- digital-ocean/main.tf
+++ digital-ocean/main.tf
@@ -17,17 +17,31 @@ resource "digitalocean_tag" "teste" {
   name = "teste"
 }
 
+# Example just to ilustrate how to use vars to pass to vm count
+variable "number_of_servers" {
+  default = "1"
+}
+
 resource "digitalocean_droplet" "grafino_tf" {
   # Obtain your ssh_key id number via your account. See Document https://developers.digitalocean.com/documentation/v2/#list-all-keys
-  ssh_keys           = [5343154]            # Key example
-  image              = "${var.centos}"
+  ssh_keys = [5343154] # Key example
+
+  # ssh_keys         = If you want to use several keys use the format below
+  # ssh_keys         = If you want you can use only the new generated key pair.
+  # ssh_keys         = [5343154, "${digitalocean_ssh_key.test-key.id}"]
+  image = "${var.centos}"
+
   region             = "${var.do_lon1}"
   size               = "512mb"
   private_networking = false
   backups            = false
   ipv6               = false
-  name               = "grafino.test.local"
 
+  # Count index exists for increment values in vm creation
+  name  = "grafino.test-${count.index}.local"
+  count = "${var.number_of_servers}"
+
+  # Example on how to include the tags defined above
   tags = [
     "${digitalocean_tag.grafino.id}",
     "${digitalocean_tag.teste.id}",
@@ -47,6 +61,13 @@ resource "digitalocean_droplet" "grafino_tf" {
     }
   }
 }
+
+# Create an aditional SSH key for acessing the vm.
+/*resource "digitalocean_ssh_key" "test-key" {
+  name       = "Aditional Key for access the vm"
+  public_key = "${file("./test-key.pub")}"
+}*/
+
 
 /*resource "digitalocean_domain" "grafino_tf" {
   name       = "www.grafino_tf.com"
