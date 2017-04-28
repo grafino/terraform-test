1b83cbe916d59cfd0ccd83c687c7166dbb91d363
Rui Grafino <rui.grafino@marionete.co.uk>
2017-04-28 16:59:02 +0100

    Delete old digitalocean folder

--- digital-ocean/API-wrapper/get_keys.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/account/keys" | python -m json.tool
--- digital-ocean/output.tf
+++
@@ -1,9 +0,0 @@
-# Two diferent ways to output list when creating more than one vm
-
-output "Name" {
-  value = ["${digitalocean_droplet.grafino_tf.*.name}"]
-}
-
-output "Public ip" {
-  value = "${join(",", digitalocean_droplet.grafino_tf.*.ipv4_address)}"
-}
